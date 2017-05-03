88aff868625fe7fd8228d2b1de4e117c0407b09e
sebastianciocan <sebastianciocan@evozon.com>
2017-05-03 15:12:17 +0300

    New branch stuff

---
+++ src/com/warehouse/data/NewClass.java
@@ -0,0 +1,11 @@
+package com.warehouse.data;
+
+/**
+ * Created by sebastianciocan on 5/3/2017.
+ */
+public class NewClass {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Hey!");
+    }
+}
